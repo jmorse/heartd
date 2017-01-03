dfde8555bcbcb66469dcc9069e44eaeb6f98e002
Jeremy Morse <jmorse+git@studentrobotics.org>
2017-01-03 23:57:11 +0000

    Allow for self rate limiting of how fast we read
    So that one can reply signals at different speeds

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import argparse
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -8,6 +9,11 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as MplBackend
 
 from heart import Heart
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--rate-limit", help="Limit rate at which we read samples", action='store_true')
+
+args = parser.parse_args()
 
 SAMPLES=3000
 MINY=300
@@ -52,11 +58,27 @@ class Display(QMainWindow):
         self.ax.clear()
         self.ax.set_ylim(MINY, MAXY)
 
+    def read_pacer(self):
+        if args.rate_limit == None:
+            return lambda: True
+
+        self.pacer_count = 0
+
+        def pacer_func():
+            if self.pacer_count >= FRAMERATE:
+                return False
+            else:
+                self.pacer_count += 1
+                return True
+
+        return pacer_func
+
     def update(self):
         global data # yeah
 
         newsamples = []
-        while True:
+        pacer = self.read_pacer()
+        while pacer():
             samp = heart.read_sample()
             if samp == None:
                 break

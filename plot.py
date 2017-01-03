45b9e94614cf6bd6c38b1bec5962db91ada31729
Jeremy Morse <jmorse+git@studentrobotics.org>
2017-01-03 01:02:17 +0000

    Vomit QT GUI code everywhere

--- plot.py
+++ plot.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python
 
 import sys
-import matplotlib.pyplot as plt
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as MplBackend
+
+class Display(QMainWindow):
+    def __init__(self, data, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.setWindowTitle('halp')
+
+        self.frame = QWidget()
+
+        self.mpl_fig = Figure((5, 5))
+        self.mpl_surface = MplBackend(self.mpl_fig)
+        v = QVBoxLayout()
+        v.addWidget(self.mpl_surface)
+        self.frame.setLayout(v)
+        self.setCentralWidget(self.frame)
+
+        self.ax = self.mpl_fig.add_subplot(1, 1, 1)
+        self.ax.plot(data)
 
 data = []
 with open(sys.argv[1], "r") as f:
@@ -14,5 +35,10 @@ with open(sys.argv[1], "r") as f:
         x = [int(y) for y in x]
         data.append(x[1])
 
-plt.plot(data)
-plt.show()
+#plt.plot(data)
+#plt.show()
+
+app = QApplication(sys.argv)
+d = Display(data)
+d.show()
+app.exec_()

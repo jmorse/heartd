b96537d99e9cfdc644b19136031e1bcc1276dfb0
Jeremy Morse <jmorse+git@studentrobotics.org>
2017-01-05 20:37:27 +0000

    Support sucking information out of dumpfiles
    To numpy

--- heart.py
+++ heart.py
@@ -1,3 +1,5 @@
+import numpy as np
+import os
 import socket
 import struct
 
@@ -40,6 +42,7 @@ class HeartRecording(Heart):
     def __init__(self, f):
         super(HeartRecording, self).__init__()
         self.thefile = open(f, "rb")
+        self.filename = f
 
     def connect(self):
         pass
@@ -49,3 +52,69 @@ class HeartRecording(Heart):
         if len(b) == 0:
             return None
         return Sample.from_bytes(b)
+
+    def to_numpy(self):
+        res = os.stat(self.filename)
+        size = res.st_size
+        assert (size % 8) == 0
+
+        # Problem: there are certain skips in the signal, roughly once ever 32
+        # samples (but never double skips). Handle by repeating the prior
+        # sample for now. But, this means we don't know the size in advance, so
+        # can't allocate one np array to rule them all.
+
+        # Burn first sample
+        s = self.read_sample()
+        prevtime = s.timestamp
+
+        def combiner():
+            allarray = None
+            while True:
+                newarray = np.empty(65536, np.int16)
+                x = 0
+                while x < 65536:
+                    foo = yield None
+
+                    # Check for breakout
+                    if foo == None:
+                        if x == 0:
+                            yield allarray
+
+                        # Trim newarray
+                        newarray = newarray[0:x]
+                        # Append to allarray
+                        if allarray is None:
+                            allarray = newarray
+                        else:
+                            allarray = np.concatenate((allarray, newarray))
+                        yield allarray
+
+                    # Received a sample -> store
+                    newarray[x] = foo
+                    x += 1
+
+                # End of 2^16 loop...
+                if allarray is None:
+                    allarray = newarray
+                else:
+                    allarray = np.concatenate((allarray, newarray))
+
+        gen = combiner()
+        gen.next()
+        while True:
+                s = self.read_sample()
+                if s == None:
+                    break
+
+                if s.timestamp != prevtime + 1:
+                    # Must only ever skip by 1
+                    assert s.timestamp == (prevtime + 2) % 65536
+                    gen.send(s.value)
+                    gen.send(s.value)
+                else:
+                    gen.send(s.value)
+
+                prevtime = s.timestamp
+
+        allarray = gen.send(None)
+        return allarray
--- process.py
+++ process.py
@@ -7,9 +7,13 @@ from heart import HeartRecording, Sample
 
 heart = HeartRecording(sys.argv[1])
 
+# Drops first sample...
+refsample = heart.read_sample()
+prevtime = refsample.timestamp
+
+fail = 0
 l = []
-while True:
-    s = heart.read_sample()
-    if s == None:
-        break;
-    l.append(s.value)
+pair= False
+s = heart.to_numpy()
+
+print "fails: {}".format(fail)

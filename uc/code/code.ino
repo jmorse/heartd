27bc7ffec0885f834af0b39d9c7f6d5ae82694f9
Jeremy Morse <jmorse+git@studentrobotics.org>
2017-01-10 00:15:01 +0000

    Pump out samples veerrryyy fast now

--- uc/code/code.ino
+++ uc/code/code.ino
@@ -1,27 +1,32 @@
 
 void setup() {
-  Serial.begin(115200);
-  analogReference(EXTERNAL);
+  Serial.begin(230400);
+//  UCSR0B |= 2; // Set U2Xn
+//  UBRR0H = 0;
+//  UBRR0L = 8;
+  //analogReference(EXTERNAL);
+  ADMUX = 0x4; // Internal reference, no left shift, ADC4.
+  ADCSRA = 0xAF; // ADC on, Auto trigger, intr enable, CLK/128 = 125khz
+  ADCSRB = 0; // Free running mode
+  ADCSRA = 0xEF; // Trigger start of free running mode
 }
 
-void loop() {  
-  // print labels
-  unsigned long t = millis();
-  while (true) {
-    //Serial.print("Hello world\n");       // prints a label
-    int val = analogRead(A4);
-    Serial.write(val & 0xFF);
-    Serial.write(val >> 8);
-    Serial.write(0xAA);
-    Serial.write(0xAA);
-    Serial.write(t & 0xFF);
-    Serial.write(t >> 8);
-    Serial.write(0x55);
-    Serial.write(0x55);
+static volatile bool read = false;
+static volatile uint16_t sample = 0;
 
-    // Poor mans timer
-    while (t == millis())
-      ;
-    t = millis();
+ISR(ADC_vect) {
+  read = true;
+  uint8_t tmp = ADCL;
+  uint8_t tmp2 = ADCH;
+  sample = ADCH << 8  | tmp;
+}
+
+void loop() {
+  while (true) {
+    if (read) {
+      read = false;
+      Serial.write(sample & 0xFF);
+      Serial.write(sample >> 8);
+    }
   }
 }
